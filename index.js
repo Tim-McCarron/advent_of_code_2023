8433fa71f88ed29e26b229213298bb7cfbf97d77
abe <timmccarron@hotmail.co.uk>
2023-12-03 13:19:08 -0400

    PART TWO BABY!

--- index.js
+++ index.js
@@ -1,6 +1,60 @@
 // sup gangstas
 const fs = require('fs')
 
+const ORDER = {
+  FIRST: 'FIRST',
+  LAST: 'LAST',
+}
+
+const NUMBOS = [
+  'zero',
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+]
+
+const getLetterPyschoMode = (dumbString, order) => {
+  if (order === ORDER.FIRST) {
+    let wordIndex = Infinity
+    let wordCursor = null
+    const numIndex = dumbString.search(/[0-9]/)
+
+    NUMBOS.forEach((num, index) => {
+      const thisNumWordIndex = dumbString.indexOf(num)
+      if (thisNumWordIndex !== -1 && thisNumWordIndex < wordIndex) {
+        wordIndex = thisNumWordIndex
+        wordCursor = index
+      }
+    })
+
+    return wordIndex < numIndex ? `${wordCursor}` : dumbString[numIndex]
+  }
+
+  if (order === ORDER.LAST) {
+    let wordIndex = -1
+    let wordCursor = null
+    const numIndex = dumbString.search(/([0-9])([a-z|A-Z])*$/)
+
+    NUMBOS.forEach((num, index) => {
+      const thisNumWordIndex = dumbString.lastIndexOf(num)
+      if (thisNumWordIndex > wordIndex) {
+        wordIndex = thisNumWordIndex
+        wordCursor = index
+      }
+    })
+
+    return wordIndex > numIndex ? `${wordCursor}` : dumbString[numIndex]
+  }
+
+  throw new Error('you frigged up big time bub!')
+}
+
 fs.readFile('./input.txt', 'utf8', (err, data) => {
   if (err) {
     console.error(err)
@@ -20,4 +74,21 @@ fs.readFile('./input.txt', 'utf8', (err, data) => {
   }, 0)
 
   console.log(`Here is the FINAL CALIBUNGUS ${finalCalibungus}`)
+
+  const partTwoGuyNumberHeaven = calibungos.reduce(
+    (accumulatingBozo, currentBozo) => {
+      return (
+        accumulatingBozo +
+        parseInt(
+          `${getLetterPyschoMode(
+            currentBozo,
+            ORDER.FIRST
+          )}${getLetterPyschoMode(currentBozo, ORDER.LAST)}`
+        )
+      )
+    },
+    0
+  )
+
+  console.log(`part 2: ${partTwoGuyNumberHeaven}`)
 })
